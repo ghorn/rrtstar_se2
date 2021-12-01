9b91b7e0529f0f515210161f13af0e0955029371
Greg Horn <greg@kittyhawk.aero>
2021-11-30 16:07:33 -0800

    Add skeleton visualization binary.

--- WORKSPACE
+++ WORKSPACE
@@ -1,6 +1,7 @@
 workspace(name = "rrtstar_se2")
 
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
 
 # googletest
 http_archive(
@@ -8,4 +9,15 @@ http_archive(
     sha256 = "b4870bf121ff7795ba20d20bcdd8627b8e088f2d1dab299a031c1034eddc93d5",
     strip_prefix = "googletest-release-1.11.0",
     urls = ["https://github.com/google/googletest/archive/release-1.11.0.tar.gz"],
+)
+
+#local_repository(
+#    name = "bb3d",
+#    path = "/home/greg/bb3d",
+#)
+git_repository(
+    name = "bb3d",
+    remote = "https://github.com/ghorn/bb3d.git",
+    commit = "799334d625dcd3a5b362a3f62c618fcd3ccb80d8",
+    shallow_since = "1638316800 -0800",
 )
---
+++ vis.cpp
@@ -0,0 +1,77 @@
+#include <bits/exception.h>  // for exception
+#include <sys/types.h>       // for key_t, uint
+
+#include <algorithm>           // for copy, max
+#include <chrono>              // for operator""s, chrono_literals
+#include <cstdio>              // for fprintf, stderr
+#include <cstdlib>             // for EXIT_SUCCESS
+#include <eigen3/Eigen/Dense>  // for Matrix, DenseCoeffsBase
+#include <functional>          // for function
+#include <iostream>            // for operator<<, basic_ostream, cerr, endl, ostream, cha...
+#include <mutex>               // for mutex, lock_guard
+#include <optional>            // for optional, nullopt
+#include <queue>               // for queue
+#include <thread>              // for sleep_for, thread
+#include <vector>              // for vector
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>  // for glfwDestroyWindow, glfwGetWindowSize, glfwPollEv...
+
+#include <glm/glm.hpp>  // for mat4
+#include <nlopt.hpp>    // for opt, LN_NELDERMEAD
+
+#include "bb3d/assert.hpp"            // for ASSERT
+#include "bb3d/opengl_context.hpp"    // for Window
+
+int run_it(char *argv0) {
+  // Boilerplate
+  bb3d::Window window(argv0);
+
+//// problem
+//ProblemVisualization visualization;
+//visualization.Update<NU_OBJ, NV_OBJ, NU_VIS, NU_VIS>(Backboard<NX, NY>::Initialize());
+//
+//// it's theadn' time
+//SharedData shared_data;
+//std::thread thread_object([&shared_data]() { Optimize(shared_data); });
+//
+//std::function<void(key_t)> handle_keypress = [&visualization](key_t key) {
+//  visualization.HandleKeyPress(key);
+//};
+//
+//std::function<void()> update_visualization = [&visualization, &shared_data]() {
+//  // drain the queue
+//  std::optional<Eigen::Matrix<double, NX, NY>> dvs = std::nullopt;
+//  {
+//    const std::lock_guard<std::mutex> lock(shared_data.queue_mutex);
+//    while (shared_data.dvs_queue.size() > 1) {
+//      shared_data.dvs_queue.pop();
+//    }
+//    if (!shared_data.dvs_queue.empty()) {
+//      dvs = shared_data.dvs_queue.front();
+//      shared_data.dvs_queue.pop();
+//    }
+//  }
+//  if (dvs) {
+//    visualization.Update<NU_OBJ, NV_OBJ, NU_VIS, NU_VIS>(
+//        Backboard<NX, NY>::ToControlPoints(*dvs));
+//  }
+//};
+//
+//std::function<void(const glm::mat4 &, const glm::mat4 &)> draw_visualization =
+//    [&visualization](const glm::mat4 &view, const glm::mat4 &proj) {
+//      visualization.Draw(view, proj);
+//    };
+//
+//window.Run(handle_keypress, update_visualization, draw_visualization);
+
+  return EXIT_SUCCESS;
+}
+
+int main(int argc __attribute__((unused)), char *argv[]) {
+  try {
+    run_it(argv[0]);
+  } catch (const std::exception &e) {
+    std::cerr << e.what();
+  }
+}
