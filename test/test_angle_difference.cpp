2bc07364b15909782467ede654319c93d8104515
Greg Horn <greg@kittyhawk.aero>
2021-12-05 22:24:49 -0800

    se2 skeleton, just missing dubins and visualizer

--- test/test_angle_difference.cpp
+++ test/test_angle_difference.cpp
@@ -3,22 +3,26 @@
 
 #include <gtest/gtest.h>
 
-#include "src/se2_point.hpp"
+#include "src/space/se2.hpp"
+
+namespace rrts::space {
 
 TEST(AngleDifference, Identities) {
-  EXPECT_EQ(Se2::AngleDifference(  M_PI,     0),  -M_PI);
-  EXPECT_EQ(Se2::AngleDifference( -M_PI,     0),   M_PI);
-  EXPECT_EQ(Se2::AngleDifference(     0,  M_PI),   M_PI);
-  EXPECT_EQ(Se2::AngleDifference(     0, -M_PI),  -M_PI);
+  EXPECT_EQ(se2::AngleDifference(  M_PI,     0),  -M_PI);
+  EXPECT_EQ(se2::AngleDifference( -M_PI,     0),   M_PI);
+  EXPECT_EQ(se2::AngleDifference(     0,  M_PI),   M_PI);
+  EXPECT_EQ(se2::AngleDifference(     0, -M_PI),  -M_PI);
 }
 
 TEST(AngleDifference, half_pi) {
-  EXPECT_EQ(Se2::AngleDifference(1.5*M_PI,     M_PI),  1.5707963267948966);
-  EXPECT_EQ(Se2::AngleDifference(3.5*M_PI,     M_PI),  1.5707963267948966);
-  EXPECT_EQ(Se2::AngleDifference(5.5*M_PI,     M_PI),  1.5707963267948948);
-  EXPECT_EQ(Se2::AngleDifference(7.5*M_PI,     M_PI),  1.5707963267948948);
-  EXPECT_EQ(Se2::AngleDifference(    M_PI, 1.5*M_PI), -1.5707963267948966);
-  EXPECT_EQ(Se2::AngleDifference(    M_PI, 3.5*M_PI), -1.5707963267948966);
-  EXPECT_EQ(Se2::AngleDifference(    M_PI, 5.5*M_PI), -1.5707963267948948);
-  EXPECT_EQ(Se2::AngleDifference(    M_PI, 7.5*M_PI), -1.5707963267948948);
+  EXPECT_EQ(se2::AngleDifference(1.5*M_PI,     M_PI),  1.5707963267948966);
+  EXPECT_EQ(se2::AngleDifference(3.5*M_PI,     M_PI),  1.5707963267948966);
+  EXPECT_EQ(se2::AngleDifference(5.5*M_PI,     M_PI),  1.5707963267948948);
+  EXPECT_EQ(se2::AngleDifference(7.5*M_PI,     M_PI),  1.5707963267948948);
+  EXPECT_EQ(se2::AngleDifference(    M_PI, 1.5*M_PI), -1.5707963267948966);
+  EXPECT_EQ(se2::AngleDifference(    M_PI, 3.5*M_PI), -1.5707963267948966);
+  EXPECT_EQ(se2::AngleDifference(    M_PI, 5.5*M_PI), -1.5707963267948948);
+  EXPECT_EQ(se2::AngleDifference(    M_PI, 7.5*M_PI), -1.5707963267948948);
+}
+
 }
