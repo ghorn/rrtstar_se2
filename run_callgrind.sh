34befcd737ee44633deac9b37f33ae054ff17418
Greg Horn <gregmainland@gmail.com>
2023-02-07 02:07:06 -0800

    minor

--- run_callgrind.sh
+++ run_callgrind.sh
@@ -4,6 +4,6 @@ set -e
 rm -f callgrind.out.*
 bazel build -c dbg --strip=never --copt="-g" //...
 #valgrind --tool=callgrind ./bazel-bin/run_for_profiling
-valgrind --tool=callgrind ./bazel-bin/test_fast_r3
-rm -f test_fast_r3
+valgrind --tool=callgrind ./bazel-bin/test_fast_tree
+rm -f test_fast_tree
 kcachegrind callgrind.out.*
