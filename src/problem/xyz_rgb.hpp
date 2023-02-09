c972dea7610526fc3522cf0458e2cf75ee8444d1
Greg Horn <gregmainland@gmail.com>
2023-02-09 10:41:20 -0800

    Remove unused constructors.

--- src/problem/xyz_rgb.hpp
+++ src/problem/xyz_rgb.hpp
@@ -11,10 +11,6 @@ struct XyzRgb {
   float b;
   float a;
   XyzRgb() = default;
-  XyzRgb(float x_, float y_, float z_, float r_, float g_, float b_, float a_)
-      : x(x_), y(y_), z(z_), r(r_), g(g_), b(b_), a(a_){};
-  XyzRgb(const glm::vec3 &xyz, float r_, float g_, float b_, float a_)
-      : x(xyz.x), y(xyz.y), z(xyz.z), r(r_), g(g_), b(b_), a(a_){};
   XyzRgb(const glm::vec3 &xyz, const glm::vec4 &rgba)
       : x(xyz.x), y(xyz.y), z(xyz.z), r(rgba.r), g(rgba.g), b(rgba.b), a(rgba.a){};
 };
